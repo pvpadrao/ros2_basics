6fb4ede52a079cd92e14c998f84ebe6460b8cd96
Paulo Padrao <55985904+pvpadrao@users.noreply.github.com>
2023-02-09 13:27:39 -0500

    Add files via upload

--- my_py_pkg/my_py_pkg/my_first_node.py
+++ my_py_pkg/my_py_pkg/my_first_node.py
@@ -5,6 +5,21 @@ from rclpy.node import Node
 class MyNode(Node):
     def __init__(self):
         super().__init__("py_test")
+        self.declare_parameters(
+            namespace='',
+            parameters=[
+                ('bool_value', rclpy.Parameter.Type.BOOL),
+                ('int_number', rclpy.Parameter.Type.INTEGER),
+                ('float_number', rclpy.Parameter.Type.DOUBLE),
+                ('str_text', rclpy.Parameter.Type.STRING),
+                ('bool_array', rclpy.Parameter.Type.BOOL_ARRAY),
+                ('int_array', rclpy.Parameter.Type.INTEGER_ARRAY),
+                ('float_array', rclpy.Parameter.Type.DOUBLE_ARRAY),
+                ('str_array', rclpy.Parameter.Type.STRING_ARRAY),
+                ('bytes_array', rclpy.Parameter.Type.INTEGER_ARRAY),
+                ('nested_param.another_int', rclpy.Parameter.Type.INTEGER)
+            ]
+        )
         self.counter_ = 0
         self.get_logger().info("My First ROS Node!")
         self.create_timer(1, self.timer_callback)
